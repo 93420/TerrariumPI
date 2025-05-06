0880dedeefe7f939e11670e7611b7335ae842561
Christophe <47230138+93420@users.noreply.github.com>
2025-05-06 16:28:03 -0400

    Update install.sh
    change packages to remove

--- install.sh
+++ install.sh
@@ -36,7 +36,7 @@ fi
 
 SCRIPT_GROUP="$(id -gn ${SCRIPT_USER})"
 
-CLEANUP_PACKAGES="wolfram sonic-pi openbox nodered chromium-browser desktop-base gnome-desktop3-data libgnome-desktop epiphany-browser-data epiphany-browser nuscratch scratch wiringpi libreoffice"
+CLEANUP_PACKAGES="wolfram sonic-pi openbox nodered gnome-desktop3-data libgnome-desktop epiphany-browser-data epiphany-browser nuscratch scratch libreoffice realvnc-vnc-server libpam-chksshpwd"
 PYTHON_LIBS="python3-pip python3-dev python3-venv"
 
 PIP_MODULES=""
